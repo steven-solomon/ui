9da3a6864884c74df0d8861514d6a4d332245d4d
Chunchun <yccvivo@gmail.com>
2022-02-24 15:37:53 -0600

    refactor: convert DashboardCards into functional component

--- src/dashboards/components/dashboard_index/DashboardCards.tsx
+++ src/dashboards/components/dashboard_index/DashboardCards.tsx
@@ -1,7 +1,6 @@
 // Libraries
-import React, {PureComponent} from 'react'
+import React, {FC, useState, useEffect, useCallback} from 'react'
 import {connect} from 'react-redux'
-import memoizeOne from 'memoize-one'
 
 // Components
 import DashboardCard from 'src/dashboards/components/dashboard_index/DashboardCard'
@@ -21,7 +20,7 @@ import {extractDashboardLimits} from 'src/cloud/utils/limits'
 import {isFlagEnabled} from 'src/shared/utils/featureFlag'
 import {CLOUD} from 'src/shared/constants'
 
-let getPinnedItems
+let getPinnedItems: () => Promise<any>
 if (CLOUD) {
   getPinnedItems = require('src/shared/contexts/pinneditems').getPinnedItems
 }
@@ -38,43 +37,51 @@ interface OwnProps {
   onFilterChange: (searchTerm: string) => void
 }
 
-class DashboardCards extends PureComponent<OwnProps & StateProps> {
-  private _observer
-  private _isMounted = true
-  private _spinner
+const DashboardCards: FC<OwnProps & StateProps> = ({
+  dashboards,
+  sortDirection,
+  sortKey,
+  sortType,
+  onFilterChange,
+  limitStatus,
+}) => {
+  const [windowSize, setWindowSize] = useState(0)
+  const [pages, setPages] = useState(1)
+  const [pinnedItems, setPinnedItems] = useState([])
 
-  private memGetSortedResources = memoizeOne<typeof getSortedResources>(
-    getSortedResources
-  )
+  let _observer: IntersectionObserver
+  let _isMounted: boolean = true
+  let _spinner: Element
 
-  state = {
-    pages: 1,
-    windowSize: 0,
-    pinnedItems: [],
-  }
+  const memGetSortedResources = useCallback(getSortedResources, [
+    dashboards,
+    sortKey,
+    sortDirection,
+    sortType,
+  ])
 
-  public componentDidMount() {
+  useEffect(() => {
     if (isFlagEnabled('pinnedItems') && CLOUD) {
       getPinnedItems()
-        .then(res => {
-          if (this._isMounted) {
-            this.setState(prev => ({...prev, pinnedItems: res, windowSize: 15}))
+        .then((res: React.SetStateAction<any[]>) => {
+          if (_isMounted) {
+            setWindowSize(15)
+            setPinnedItems(res)
           }
         })
-        .catch(err => {
+        .catch((err: any) => {
           console.error(err)
         })
     } else {
-      this.setState(prev => ({...prev, windowSize: 15}))
+      setWindowSize(15)
     }
-  }
+    return function cleanup() {
+      _isMounted = false
+    }
+  }, [])
 
-  public componentWillUnmount() {
-    this._isMounted = false
-  }
-
-  private registerSpinner = elem => {
-    this._spinner = elem
+  const registerSpinner = (elem: any) => {
+    _spinner = elem
 
     if (!elem) {
       return
@@ -90,80 +97,67 @@ class DashboardCards extends PureComponent<OwnProps & StateProps> {
 
     threshold.reverse()
 
-    this._observer = new IntersectionObserver(this.measure, {
+    _observer = new IntersectionObserver(measure, {
       threshold,
       rootMargin: '60px 0px',
     })
 
-    this._observer.observe(this._spinner)
+    _observer.observe(_spinner)
   }
 
-  private measure = entries => {
+  const measure = (entries: any[]) => {
     if (
       entries
         .map(e => e.isIntersecting)
         .reduce((prev, curr) => prev || curr, false)
     ) {
-      this.setState({
-        pages: this.state.pages + 1,
-      })
+      setPages(prevPages => prevPages + 1)
     }
   }
 
-  public render() {
-    const {
-      dashboards,
-      sortDirection,
-      sortKey,
-      sortType,
-      onFilterChange,
-    } = this.props
-    const sortedDashboards = this.memGetSortedResources(
-      dashboards,
-      sortKey,
-      sortDirection,
-      sortType
-    )
+  const sortedDashboards = memGetSortedResources(
+    dashboards,
+    sortKey,
+    sortDirection,
+    sortType
+  )
 
-    const {windowSize, pages, pinnedItems} = this.state
-
-    return (
-      <div style={{height: '100%'}}>
-        <div className="dashboards-card-grid">
-          {sortedDashboards
-            .filter(d => d.status === RemoteDataState.Done)
-            .slice(0, pages * windowSize)
-            .map(({id, name, description, labels, meta}) => (
-              <DashboardCard
-                key={id}
-                id={id}
-                name={name}
-                labels={labels}
-                updatedAt={meta.updatedAt}
-                description={description}
-                onFilterChange={onFilterChange}
-                isPinned={
-                  !!pinnedItems.find(item => item?.metadata.dashboardID === id)
-                }
-              />
-            ))}
-          <AssetLimitAlert
-            className="dashboards--asset-alert"
-            resourceName="dashboards"
-            limitStatus={this.props.limitStatus}
-          />
-        </div>
-        {windowSize * pages < dashboards.length && (
-          <div
-            style={{height: '140px', margin: '14px auto'}}
-            ref={this.registerSpinner}
-          >
-            <TechnoSpinner />
-          </div>
-        )}
+  return (
+    <div style={{height: '100%'}}>
+      <div className="dashboards-card-grid">
+        {sortedDashboards
+          .filter(d => d.status === RemoteDataState.Done)
+          .slice(0, pages * windowSize)
+          .map(({id, name, description, labels, meta}) => (
+            <DashboardCard
+              key={id}
+              id={id}
+              name={name}
+              labels={labels}
+              updatedAt={meta.updatedAt}
+              description={description}
+              onFilterChange={onFilterChange}
+              isPinned={
+                !!pinnedItems.find(item => item?.metadata.dashboardID === id)
+              }
+            />
+          ))}
+        <AssetLimitAlert
+          className="dashboards--asset-alert"
+          resourceName="dashboards"
+          limitStatus={limitStatus}
+        />
       </div>
-    )
-  }
+      {windowSize * pages < dashboards.length && (
+        <div
+          style={{height: '140px', margin: '14px auto'}}
+          ref={registerSpinner}
+        >
+          <TechnoSpinner />
+        </div>
+      )}
+    </div>
+  )
 }
 
 const mstp = (state: AppState) => {
